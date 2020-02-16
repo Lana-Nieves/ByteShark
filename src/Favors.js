048c7366524aaf26c4556d215b2777fc5e735837
hanum-lee <chococloudy@gmail.com>
2020-02-15 20:13:32 -0700

    naviagtion to favor added

---
+++ src/Favors.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Post from "./Post";
+
+class Favors extends React.Component{
+
+
+    loadPost(){
+        return (ReactDOM.render(<Favor/>,document.getElementById("test2")));
+    }
+
+    render() {
+        return(
+          <div className='container'>
+              <div className={Favors}>
+                  <div className='Favor'>
+                      <div className='Title'> I need a calculator!</div>
+                      <div className='user'>Random1</div>
+                  </div>
+
+              </div>
+
+          </div>
+        );
+    }
+}
+
+class Favor extends React.Component{
+
+    render() {
+        return(
+            <div className={Favor}>
+                <div className="title"> I need a calculator!</div>
+                <div className='note'> Can I borrow one? </div>
+                <div className='UserName'> Random1</div>
+            </div>
+
+        );
+
+    }
+}
+
+export default Favors
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -7,12 +7,25 @@ import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import Nav from "react-bootstrap/Nav";
 import StudyGroup from './StudyGroup'
 import ReactDOM from "react-dom";
+import MainPage from './Apptest';
+import Favors from './Favors';
+
 
 class Navaigator extends React.Component{
 
     loadStudyGroup(){
         return(ReactDOM.render(<StudyGroup/>,document.getElementById("test2")));
     }
+
+    loadMainPage(){
+        return(ReactDOM.render(<MainPage/>,document.getElementById("root")));
+    }
+
+    loadFavor(){
+        return(ReactDOM.render(<Favors/>,document.getElementById("test2")));
+    }
+
+
     render() {
         return(
             <div className={Navaigator}>
@@ -29,7 +42,7 @@ class Navaigator extends React.Component{
                             <Nav.Link>
                                 Tips
                             </Nav.Link>
-                            <Nav.Link>
+                            <Nav.Link onClick={this.loadFavor}>
                                 Favors
                             </Nav.Link>
                             <NavDropdown id={"collasible-nav-dropdown"} title={"Menu"}>
